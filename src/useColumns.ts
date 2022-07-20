92b576d74e8562b8f02d1aaa28384f501be7106a
Kseniya <kcniyamelihova@gmail.com>
2022-07-20 14:57:09 +0300

    cleared code

--- src/useColumns.ts
+++ src/useColumns.ts
@@ -1,29 +1,24 @@
-import { useState } from "react";
-import configure from "./api/report-config.json";
+import { useState } from 'react';
+import configure from './api/report-config.json';
 
-export const stringNames = ["dataField", "caption", "format"] as const;
 export const dataType = [
-  "number",
-  "boolean",
-  "string",
-  "date",
-  "datetime",
+  'number',
+  'boolean',
+  'string',
+  'date',
+  'datetime',
 ] as const;
-export const alignmentType = ["right", "center", "left"] as const;
+export const alignmentType = ['right', 'center', 'left'] as const;
 
-export type StringFieldName = typeof stringNames[number];
 export type DataType = typeof dataType[number];
 export type AlignmentType = typeof alignmentType[number];
 
-export function isStringFieldName(name: string): name is StringFieldName {
-  return (stringNames as ReadonlyArray<string>).includes(name);
-}
-export function isFormatType(name: string): name is DataType {
-  return (dataType as ReadonlyArray<string>).includes(name);
-}
-export function isAlignmentType(name: string): name is AlignmentType {
-  return (alignmentType as ReadonlyArray<string>).includes(name);
-}
+// export function isFormatType(name: string): name is DataType {
+//   return (dataType as ReadonlyArray<string>).includes(name);
+// }
+// export function isAlignmentType(name: string): name is AlignmentType {
+//   return (alignmentType as ReadonlyArray<string>).includes(name);
+// }
 
 export interface IColumns {
   dataField: string;
